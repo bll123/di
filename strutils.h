745b71fa129933954e90fa8edd969a37e52870dc
Brad Lanam <brad.lanam.di@gmail.com>
2025-01-08 10:52:28 -0800

    wip: re-work: cleanup

--- strutils.h
+++ strutils.h
@@ -1,5 +1,5 @@
-#ifndef INC_STRUTILS_H
-#define INC_STRUTILS_H
+#ifndef DI_INC_STRUTILS_H
+#define DI_INC_STRUTILS_H
 
 #include "config.h"
 
@@ -7,19 +7,19 @@
 extern "C" {
 # endif
 
-extern void * di_realloc            (void *, Size_t);
-extern void di_trimchar                (char *, int);
+extern void * di_realloc (void *, Size_t);
+extern void di_trimchar (char *, int);
 
 # if ! _lib_strdup
-extern char *strdup                 (const char *);
+extern char * strdup (const char *);
 # endif
 
 # if ! _lib_strstr
-extern char *strstr           (const char *, const char *);
+extern char * strstr (const char *, const char *);
 # endif
 
 # if defined (__cplusplus) || defined (c_plusplus)
 }
 # endif
 
-#endif /* INC_STRUTILS_H */
+#endif /* DI_INC_STRUTILS_H */
